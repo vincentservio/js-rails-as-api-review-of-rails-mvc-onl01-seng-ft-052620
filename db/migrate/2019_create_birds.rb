c890ca4c96fe0d2537b5d04507004bd94e2a82e7
Vincent Servio <vincentservio@gmail.com>
2020-09-01 21:49:33 +0000

    Automatically backed up by Learn

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,3 +1,11 @@
 # Add code from Readme
-
-git@github.com:vincentservio/fewpjs-build-the-example-onl01-seng-ft-052620.git
+class CreateBirds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+ 
+      t.timestamps
+    end
+  end
+end
